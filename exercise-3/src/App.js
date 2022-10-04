1237745a59584c6968a7ccedd9981e077854a5f6
Suning Yao <sy2940@nyu.edu>
2022-10-04 09:43:29 -0400

    Call weather api

--- exercise-3/src/App.js
+++ exercise-3/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './pages/home';
+import Home from './pages/Home';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
--- exercise-3/src/pages/home.js
+++ exercise-3/src/pages/home.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { WEATHER_APP_API_KEY } from '../API_KEYS';
+
+const openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=${WEATHER_APP_API_KEY}`;
 
 const Home = () => {
+  const [weatherData, setWeatherData] = React.useState({});
+  useEffect(() => {
+    axios
+      .get(openWeatherURL)
+      .then((res) => {
+        setWeatherData(res.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        setWeatherData({});
+      });
+  }, []);
+
   return (
     <main>
       <h1>Weather App</h1>
+      <p>Humidity: {weatherData.name}</p>
     </main>
   );
-}
+};
 
 export default Home;

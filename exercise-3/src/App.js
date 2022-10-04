81170ae88ec544fb5765f0109fe2c35c0ab1c6de
Suning Yao <sy2940@nyu.edu>
2022-10-04 08:30:57 -0400

    Init exercise-3

---
+++ exercise-3/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from './pages/home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ exercise-3/src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Weather App</h1>
+    </main>
+  );
+}
+
+export default Home;

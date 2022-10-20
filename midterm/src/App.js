913736ee0c8c36d39b37e8dcc1bd1de118577cca
Suning Yao <sy2940@nyu.edu>
2022-10-20 08:26:51 -0400

    Remove console logs

--- midterm/src/App.js
+++ midterm/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Search from './components/Search';
 import Card from './components/Card';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { addresses } from './addresses';
 import { fetchRender, fetchData } from './utils/api';
 import getRandomInt from './utils/random';
@@ -14,22 +14,22 @@ function App() {
   // get random nft address
   const randomAddress = () => {
     setAddress(addresses[getRandomInt(3)]);
-  }
+  };
 
   // update the nft address state
   const updateAddress = (newAddress) => {
     setAddress(newAddress);
-  }
+  };
 
   // get random nft token id
   const randomId = () => {
     return getRandomInt(100);
-  }
+  };
 
   // update the nft token id state
   const updateId = () => {
     setId(randomId());
-  }
+  };
 
   // fetch all data and render
   async function fetchNFT() {
@@ -38,22 +38,23 @@ function App() {
     const attributes = await data.metadata.attributes;
     const url = await data.url;
     const name = await data.name;
-    const collectionName = await data.collectionName
-    console.log(data)
-    setNFT({media, attributes, name, url, collectionName});
+    const collectionName = await data.collectionName;
+    setNFT({ media, attributes, name, url, collectionName });
   }
 
   useEffect(() => {
-    randomAddress()
+    randomAddress();
   }, []);
 
-  useEffect(() => {
-    console.log(nft);
-  }, [nft]);
-
   return (
-    <div className="App">
-      <Search updateAddress={updateAddress} randomAddress={randomAddress} updateId={updateId} address={address} fetchNFT={fetchNFT}/>
+    <div className='App'>
+      <Search
+        updateAddress={updateAddress}
+        randomAddress={randomAddress}
+        updateId={updateId}
+        address={address}
+        fetchNFT={fetchNFT}
+      />
       <Card nft={nft} address={address} id={id} />
     </div>
   );

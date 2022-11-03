f76f8372c1c91f4b3768479c8fd2d960f27925cd
Suning Yao <sy2940@nyu.edu>
2022-11-03 10:09:09 -0400

    finally get port working

--- exercise-5/app.js
+++ exercise-5/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 4000;
+const port = process.env.PORT;
 
 const indexPage = require('./routes/index.js');
 

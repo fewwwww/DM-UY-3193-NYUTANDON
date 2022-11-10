5dd8a00d5f4a28c37eb3bec6f2ef5e4d5929c953
Suning Yao <sy2940@nyu.edu>
2022-11-10 08:47:13 -0500

    finish create post function

--- exercise-5/routes/createPost.js
+++ exercise-5/routes/createPost.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const firestore = require('firebase/firestore');
+const db = firestore.getFirestore();
 
 const createPostForm = `
 <h1>Create Post</h1>
-<form action="">
-  <input type="text" name="title" placeholder="Title"/>
-  <input type="text" name="text" placeholder="Text"/>
-  <input type="text" name="author" placeholder="Author"/>
-  <button type="submit">Submit</button>
+<form action="/create/submit">
+  <div style="display: flex; flex-direction: column; max-width: 400px;">
+    <label for="postTitle">Title</label>
+    <input type="text" name="postTitle" placeholder="Title"/>
+    <label for="postText">Text</label>
+    <input type="text" name="postText" placeholder="Text"/>
+    <label for="postAuthor">Author</label>
+    <input type="text" name="postAuthor" placeholder="Author"/>
+    <button type="submit">Submit</button>
+  </div>
 </form>
 `;
 
@@ -18,6 +25,36 @@ router.use((req, res, next) => {
 
 router.get('/', (req, res) => {
   res.send(createPostForm);
+});
+
+router.get('/submit', (req, res) => {
+  const queryParams = req.query;
+  const title = queryParams.postTitle;
+  const text = queryParams.postText;
+  const author = queryParams.postAuthor;
+  const idFromTitle = title.replace(/\s+/g, '-').toLowerCase();
+
+  const setBlogPost = firestore.setDoc(
+    firestore.doc(db, 'posts', idFromTitle),
+    {
+      title: title,
+      text: text,
+      author: author,
+    },
+  );
+
+  setBlogPost
+    .then((response) => {
+      return res.send(`
+      <h1>Submission Successful!</h1>
+      <p><a href="/create">Add Another Post</a></p>
+      <p><a href="/">Return Home</a></p>
+    `);
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.send(error);
+    });
 });
 
 module.exports = router;

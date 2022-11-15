68d828687a0fd86c6a7e43c95f207339be66cef1
Suning Yao <sy2940@nyu.edu>
2022-11-15 09:42:34 -0500

    Add template for auth

--- exercise-6/src/App.js
+++ exercise-6/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import { useEffect, useState } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+import './App.css';
 import CreateUserPage from './pages/CreateUser';
 import LoginPage from './pages/Login';
 import UserProfilePage from './pages/UserProfile';
 import Header from './components/Header';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDAISmqw2SxGzsW5dLGBnDUJaqOhQoiCoo',
+  authDomain: 'exercise-6-6e5cf.firebaseapp.com',
+  projectId: 'exercise-6-6e5cf',
+  storageBucket: 'exercise-6-6e5cf.appspot.com',
+  messagingSenderId: '586814722468',
+  appId: '1:586814722468:web:1634ff2b89d1b8ff6d3131',
+};
 
 const router = createBrowserRouter([
   {
@@ -21,9 +34,38 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const [appInitialized, setAppInitialized] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+  const [userInformation, setUserInformation] = useState({});
+
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+    setAppInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (appInitialized) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // user signed in, see content
+          setUserInformation(user);
+          setIsLoggingIn(true);
+        } else {
+          // user signed out
+          setUserInformation({});
+          setIsLoggingIn(false);
+        }
+        // whenever state changes setIsLoading to false
+        setIsLoading(false);
+      });
+    }
+  }, [appInitialized]);
+
   return (
     <div className='App'>
-      <Header/>
+      <Header />
       <RouterProvider router={router} />
     </div>
   );
